2e6f80aa1f835765645776927c2b1c5d4d52bac6
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-07 18:53:22 +0300

    connection WIP check if email exsits

--- process_signup.php
+++ process_signup.php
@@ -3,5 +3,42 @@
 include('connection.php');
 
 if(isset($_POST['signup_btn']))
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+
+    if ($password != $password_confirm)
+    {
+        header('location: signup.php?error_message=passwords do not match');
+        exit;
+    }
+
+    $stmt = $conn->prepare(
+    "SELECT id 
+    FROM users 
+    WHERE username = ? OR email = ?");
+    
+    $stmt->bind_param("ss", $username, $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if($stmt->num_rows() > 0)
+    {
+        header("location: signup.php?error_message=User already exists");
+        exit;
+    }
+    else
+    {
+        $stmt =  $conn->prepare(
+            "INSERT INTO users (username,email,password)"
+    }
+}
+else
+{
+
+
+}
 
 ?>
