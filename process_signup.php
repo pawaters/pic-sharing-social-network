f18fa0c761257810653343f0709ad71f0791f96a
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-29 14:03:36 +0300

    process_signup

--- process_signup.php
+++ process_signup.php
@@ -22,6 +22,8 @@ if(isset($_POST['signup_btn']))
     
     $stmt->bind_param("ss", $username, $email);
     $stmt->execute();
+    //store_result : downloads all rows and when you fetch first row. 
+    //next fetch() calls will iterate without having to call a loop.
     $stmt->store_result();
 
     if($stmt->num_rows() > 0)
@@ -32,12 +34,49 @@ if(isset($_POST['signup_btn']))
     else
     {
         $stmt =  $conn->prepare(
-            "INSERT INTO users (username,email,password)"
+            "INSERT INTO users (username,email,password) 
+            VALUES (?,?,?)");
+        $stmt->bind_param("sss", $username, $email, md5($password));
+        
+        //if user created successfully, return user info
+        if ($stmt->execute())
+        {
+            //select from database the lines that match, the following info
+            $stmt = $conn->prepare("SELECT id, username, email, image, following, followers, posts
+            FROM users 
+            WHERE username = ?");
+            //obligatory when using prepare, define var names for each var
+            $stmt->bind_param("s", $username);
+            //obligatory to run it
+            $stmt->execute();
+            //now we have the data as PHP array stored in my SQL, lets save each as a variable in PHP
+            $stmt->bind_result($id, $username, $email, $image, $following, $followers, $posts);
+            //fetch() stores the result (no need to iterate thanks to previous store_result)
+            $stmt->fetch();
+            
+            //store user info in the session
+            $_SESSION['id'] = $id;
+            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
+            $_SESSION['image'] = $image;
+            $_SESSION['following'] = $following;
+            $_SESSION['followers'] = $followers;
+            $_SESSION['posts'] = $posts;
+
+            //return to homepage
+            header("location: index.php");
+            
+        }
+        else
+        {
+            header("location: signup.php?error_message=error occurred");
+            exit;
+        }
     }
 }
 else
 {
-
+    header("location: signup.php?error_message=error occurred");
 
 }
 
