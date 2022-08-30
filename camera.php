a5e9c536da4eac1e20a5d15dacbf681d4373962a
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-30 15:14:34 +0300

    refactored camera.php with header.php include

---
+++ camera.php
@@ -0,0 +1,23 @@
+<?php include("header.php"); ?>
+    <div class="camera-container">
+        <div class="camera">
+            <div class="camera-image">
+                <img src="assets/img/rugby1.jpeg" alt="">
+                <form class="camera-form" action="">
+                    <div class="form-group">
+                        <input type="text" name="caption" class="form-control" placeholder="type caption ..." >
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="hashtags" class="form-control" placeholder="type hashtags ..." >
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="upload_image-btn" class="upload-btn">Post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
