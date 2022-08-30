196178abce89d27555658c277e7cd5e0ad5d04cc
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-30 10:48:18 +0300

    if not logged in, send back to login page

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+session_start();
+
+// if not logged in, send to login page
+if(!isset($_SESSION['id'])){
+    header('location: login.php');
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
