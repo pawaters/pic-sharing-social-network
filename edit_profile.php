e993f55c52b6370c12e1436858da91caddc4a9df
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-30 11:36:14 +0300

    refactor edit_profile

--- edit_profile.php
+++ edit_profile.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Profile</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/8da1d76717.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <!--2 main divs: navbar and main 
-    each main div includes a wrapper -->
-    <nav class="navbar"> 
-        <div class="nav-wrapper">
-            <img src="assets/img/logo.jpg" class="brand-img"/>
-            <form class="search-form">
-                <input type="text" class="search-box" placeholder="Search"/>
-            </form>
-            <div class="nav-items">
-                <i class="icon fa-solid fa-house-user"></i>
-                <i class="icon fas fa-plus"></i>
-                <i class="icon fas fa-heart"></i>
-                <div class="icon user-profile">
-                    <i class="fas fa-user"></i>
-                </div>
-            </div>
-        </div>
-    </nav>
+<?php include("header.php") ?>
+
      <!--main: under wrapper, two divs: left-col, right-col -->
     <section class = "main">
         <div class="wrapper">
@@ -35,7 +7,7 @@
 
             <h3>Update Profile</h3>
     <!--bootstrap for forms: form-label, label, form-control -->
-                <form>
+                <form action="update_profile.php" method="POST">
                     <div class="mb-3">
                         <img src="assets/img/profile.jpeg" class="edit-profile-image">
                         <input type="file" name="image" class="form-control">
