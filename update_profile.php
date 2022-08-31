2a3d1ee0c06a72f2f2fdc8b3ebbfe156892dcddf
pawaters <86101754+pawaters@users.noreply.github.com>
2022-08-31 09:48:28 +0300

    update_profile img bug solved

--- update_profile.php
+++ update_profile.php
@@ -1,74 +1,136 @@
 <?php
 
+
 session_start();
 
 include("connection.php");
 
-if (isset($_POST['update_profile_btn'])){
+if(isset($_POST['update_profile_btn'])){
 
-    $id = $_SESSION['id']; //id is unchageable for user
+    $user_id = $_SESSION['id'];
     $username = $_POST['username'];
     $bio = $_POST['bio'];
-    $image = $_FILES['image']['tmp_name'];
-    //check if image is empty
+    $image = $_FILES['image']['tmp_name'];  //file
+
     if($image != ""){
-        $image_name = $username.".jpeg";
-    } else {
+       $image_name = $username . ".jpeg"; //5.jpeg
+    }else{
         $image_name = $_SESSION['image'];
     }
-}
+
+
+
+    if($username != $_SESSION['username']){
+        //make sure that username is unique
+    
+        $stmt = $conn->prepare("SELECT username FROM users WHERE username = ?");
+
+        $stmt->bind_param("s",$username);
+        
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        //there is a user with this username
+        if($stmt->num_rows() > 0){
+            header("location: edit_profile.php?error_message=Username was already taken");
+            exit;
+
+        } else{
+
+
+
+            updateUserProfile($conn,$username,$bio,$image_name,$user_id,$image);
+
+            
+    
+        }
+    
+      
+
+    }else{
+
+
+    
+
+        updateUserProfile($conn,$username,$bio,$image_name,$user_id,$image);
+         
+
+    }
+
+   
+
     
 
 
-    //ensure username is unique
-    if($username != $_SESSION['username'])
-    {
-        $stmt = $conn->prepare("SELECT username
-                                FROM users
-                                WHERE username = ?");
-        //bind_param clarifies what to put for ?
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        // store result locally
-        $stmt->store_result();
+}else{
 
-        // with if num rows, check if there is an existing user
-        if($stmt->num_rows() > 0) 
-        {
-            header("location: edit_profile.php?error_message=username was already taken");
-            exit;
-        }
-        else
-        {
-        // update info, not insert or select
-        $stmt = $conn->prepare("UPDATE users 
-                                SET username = ?, bio = ?, image = ?
-                                WHERE id = ?");
-        //bind_param clarifies what to put for ?
-        $stmt->bind_param("sssi", $username, $bio, $image_name, $id);
-        
-        if ($stmt->execute()){
+    header("location: edit_profile.php?error_message?error occured, try again");
+    exit;
 
-            if ($image != ""){
-                move_uploaded_file($image, "assets/img/".$image_name);
-            }
-            
-            //update session
-            $_SESSION['username'] = $username;
-            $_SESSION['bio'] = $bio;
-            $_SESSION['image'] = $image;
-            
-            header("location: profile.php?success_message=Profile updated");
-            exit;
-        } else {
-            header("location: edit_profile.php?error_message=error");
-            exit;
-        };
-        
+
+}
+
+
+
+
+
+
+function updateUserProfile($conn,$username,$bio,$image_name,$user_id,$image){
+    $stmt = $conn->prepare("UPDATE users SET username = ?, bio = ? , image = ? WHERE id = ?");
+    $stmt->bind_param("sssi",$username,$bio,$image_name,$user_id);
+
+    if($stmt->execute()){
+
+        if($image != ""){
+             //store image in folder
+            move_uploaded_file($image,"assets/img/".$image_name);
         }
 
-    } else {
-        header("location: edit_profile.php?error_message=error");
+        //update session
+        $_SESSION['username']=$username;
+        $_SESSION['bio']=$bio;
+        $_SESSION['image']=$image_name;
+
+
+        updateProfileImageAndUsernameInPostsTable($conn,$username,$image_name,$user_id);
+
+        updateProfileImageAndUsernameInCommentsTable($conn,$username,$image_name,$user_id);
+
+        header("location: profile.php?success_message?Profile has been updated successfully");
         exit;
+
+    }else{
+        header("location: edit_profile.php?error_message?error occured, try again");
+        exit;
+
     }
+}
+
+function updateProfileImageAndUsernameInCommentsTable($conn,$username,$image_name,$user_id){
+
+
+    $stmt = $conn->prepare("UPDATE comments SET username = ?, profile_image = ?  WHERE user_id = ?");
+    $stmt->bind_param("ssi",$username,$image_name,$user_id);
+    $stmt->execute();
+
+    
+
+}
+
+
+function updateProfileImageAndUsernameInPostsTable($conn,$username,$image_name,$user_id){
+
+    $stmt = $conn->prepare("UPDATE posts SET username = ?, profile_image = ?  WHERE user_id = ?");
+    $stmt->bind_param("ssi",$username,$image_name,$user_id);
+    $stmt->execute();
+
+
+}
+
+
+
+
+
+
 ?>
