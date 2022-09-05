bb211db869768ea67f0b346c745a7812ccde73b5
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-05 12:14:09 +0300

    discover - now loads posts

--- discover.php
+++ discover.php
@@ -1,33 +1,70 @@
 <?php include('header.php'); ?>
 
-    <main>
-        <div class="discover-container">
-            <div class="gallery">
+<?php 
+
+include('connection.php'); 
+
+if(isset($_GET['page_no']) && $_GET['page_no'] != "")
+{
+    $page_no = $_GET['page_no'];
+} 
+else 
+{
+    $page_no = 1;
+}
+
+$stmt = $conn->prepare("SELECT COUNT(*) as total_posts FROM posts");
+$stmt->execute();
+$stmt->bind_result($total_posts);
+$stmt->store_result();
+$stmt->fetch();
+
+$total_posts_per_page = 6;
+
+$offset = ($page_no - 1) * $total_posts_per_page;
+
+$total_no_of_pages = ceil($total_posts / $total_posts_per_page);
+
+$stmt = $conn->prepare("SELECT * FROM posts LIMIT $offset, $total_posts_per_page"); 
+$stmt->execute();
+$posts = $stmt->get_result();
+
+?>
+
+<main>
+    <div class="discover-container">
+        <div class="gallery">
+
+            <?php foreach($posts as $post) { ?>
+
                 <div class="gallery-item">
-                    <img src="assets/img/flower.jpeg" class="gallery-image">
+                    <img src="<?php echo "assets/img/".$post['image'];?>" class="gallery-image">
                     <div class="gallery-item-info">
                         <ul>
-                            <li class="gallery-item-likes"><span class="hide-gallery-element">12</span>
+                            <li class="gallery-item-likes"><span class="hide-gallery-element"><?php echo $post['image'];?></span>
                                 <i class="fas fa-heart"></i>
                             </li>
-                            <li class="gallery-item-comments"><span class="hide-gallery-element">9</span>
-                                <i class="fas fa-comment"></i>
+                            <li class="gallery-item-comments"><span class="hide-gallery-element"></span>
+                                <a href="single_post.php?post_id=<?php echo $post['id'];?>" style="color: #fff"><i class="fas fa-comment"></i></a>
                             </li>
                         </ul>
                     </div>
                 </div>
-            </div>
+
+            <?php } ?> 
+
         </div>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-        </nav>
-    </main>
+    </div>
+    <nav aria-label="Page navigation example" style="display:flex; justify-content: center;">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+</main>
     
 
     
