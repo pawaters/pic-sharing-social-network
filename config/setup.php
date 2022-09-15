21043fbef9a277811e231b7edfc4e7b5e38926a9
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-15 15:46:42 +0300

    ready for PDO conversion tomorrow

--- config/setup.php
+++ config/setup.php
@@ -5,7 +5,17 @@
 	{
 		$conn = new PDO($DB_HOST, $DB_USER, $DB_PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE DATABASE IF NOT EXISTS user_db";
+		$sql = "CREATE DATABASE IF NOT EXISTS `users` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `username` varchar(50) NOT NULL,
+        `password` varchar(50) NOT NULL,
+        `email` varchar(50)  NOT NULL,
+        `image` text DEFAULT '1.jpeg',
+        `followers` int(11) DEFAULT 0,
+        `following` int(11) DEFAULT 0,
+        `posts` int(11) DEFAULT 0,
+        PRIMARY KEY (`id`)
+        )";
 		$conn->exec($sql);
 	}
 	catch(PDOException $e)
@@ -18,16 +28,16 @@
 	{
 		$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE TABLE IF NOT EXISTS `user_info` (
-		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		email VARCHAR(100) NOT NULL,
-		userr_name VARCHAR(50) NOT NULL,
-		pass_word VARCHAR(1000) NOT NULL,
-		activation_code VARCHAR(255) NOT NULL,
-		acti_stat int(11) NOT NULL,
-		notif_stat int(11) NOT NULL,
-		reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-		)";
+		$sql = "CREATE TABLE IF NOT EXISTS `comments` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `post_id` int(11) NOT NULL,
+            `user_id` int(11) NOT NULL,
+            `username` varchar(50) NOT NULL,
+            `profile_image` text  NOT NULL,
+            `comment_text` text  NOT NULL,
+            `date` DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+          )";
 		$conn->exec($sql);
 	}
 	catch(PDOException $e)
@@ -40,13 +50,12 @@
 	{
 		$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE TABLE IF NOT EXISTS `user_images` (
-		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		img_path TEXT NOT NULL,
-		img_name TEXT NOT NULL,
-		img_user VARCHAR(50) NOT NULL,
-		snap_shot INT(11) NOT NULL
-		)";
+		$sql = "CREATE TABLE IF NOT EXISTS `followings` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `user_id` int(11) NOT NULL,
+            `other_user_id` int(11) NOT NULL,
+              PRIMARY KEY (`id`)
+          )";
 		$conn->exec($sql);
 	}
 	catch(PDOException $e)
@@ -59,13 +68,18 @@
 	{
 		$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE TABLE IF NOT EXISTS `comments` (
-		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		user VARCHAR(50) NOT NULL,
-		msg VARCHAR(300) NOT NULL,
-		img TEXT NOT NULL,
-		submit_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-		)";
+		$sql = "CREATE TABLE IF NOT EXISTS `posts` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `user_id` int(11) NOT NULL,
+            `likes` int(11) NOT NULL,
+            `image` text  NOT NULL,
+            `caption` varchar(250) NOT NULL,
+            `hashtags` varchar(250) NOT NULL,
+            `date` DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `username` varchar(50)  NOT NULL,
+            `profile_image` text NOT NULL,
+            PRIMARY KEY (`id`) 
+          )";
 		$conn->exec($sql);
 	}
 	catch(PDOException $e)
@@ -79,11 +93,30 @@
 			$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql = "CREATE TABLE IF NOT EXISTS `likes` (
-			id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-			user VARCHAR(50) NOT NULL,
-			img TEXT NOT NULL,
-			submit_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-			)";
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `user_id` int(11) NOT NULL,
+                `post_id` int(11) NOT NULL,
+                PRIMARY KEY (`id`)
+              )";
+			$conn->exec($sql);
+		}
+		catch(PDOException $e)
+		{
+			echo $sql . "<br>" . $e->getMessage();
+		}
+		$conn = null;
+
+    try
+		{
+			$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "CREATE TABLE IF NOT EXISTS `pwdReset` (
+                pwdResetID int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                pwdResetEmail TEXT NOT NULL,
+                pwdResetSelector TEXT NOT NULL,
+                pwdResetToken LONGTEXT NOT NULL,
+                pwdResetExpires TEXT NOT NULL
+            )";
 			$conn->exec($sql);
 		}
 		catch(PDOException $e)
--- connection.php
+++ connection.php
@@ -1,6 +1,24 @@
 <?php
-
+    //to change when applying PDO everywhere based on https://github.com/Microsmosis/Camagru-42/blob/master/php/comments.php
     $conn = mysqli_connect("localhost", "root", "rootroot", "php_project")
         or die("DB connection failed");
+
+    function connect()
+	{
+        $DB_DSN = 'mysql:host=localhost;dbname=php_project';
+        $DB_USER = 'root';
+        $DB_PASSWORD = 'rootroot';
+        $DB_HOST = 'mysql:host=localhost';
+		try
+		{
+			$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch(PDOException $e)
+		{
+			echo "Connection failed: " . $e->getMessage();
+		}
+		return ($conn);
+	}
 
 ?>
