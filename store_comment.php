c996ee95d9adb12f4a4f8b2b4765effcad462577
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 16:27:31 +0300

    store_comment PDO ok

--- store_comment.php
+++ store_comment.php
@@ -12,11 +12,18 @@ if(isset($_POST['comment_btn']))
     $post_id = $_POST['post_id'];
     $comment_text = $_POST['comment_text'];
     $date = date("Y-m-d H:i:s");
-
+    $conn = connect_PDO();
     $stmt =  $conn->prepare(
         "INSERT INTO comments (post_id, user_id, username, profile_image, comment_text, date)
         VALUES (?,?,?,?,?,?)");
-    $stmt->bind_param("iissss", $post_id, $user_id, $username, $profile_image, $comment_text, $date);
+    // $stmt->bind_param("iissss", $post_id, $user_id, $username, $profile_image, $comment_text, $date);
+    $stmt->bindParam(1, $post_id, PDO::PARAM_INT);
+    $stmt->bindParam(2, $user_id, PDO::PARAM_INT);
+    $stmt->bindParam(3, $username, PDO::PARAM_STR);
+    $stmt->bindParam(4, $profile_image, PDO::PARAM_STR);
+    $stmt->bindParam(5, $comment_text, PDO::PARAM_STR);
+    $stmt->bindParam(6, $date, PDO::PARAM_STR);
+
     if($stmt->execute())
     {
         header('location: single_post.php?post_id='.$post_id."&success_message=comment submitted successfully");
