ab23dc1aac8ab5807c5928c691965bea5f2d2d85
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 14:38:26 +0300

    other_user_profile

--- get_other_user_posts.php
+++ get_other_user_posts.php
@@ -1,6 +1,6 @@
 <?php
 
-include('connection.php'); 
+// include('connection.php'); 
 
 
 
@@ -13,12 +13,15 @@ else
     $page_no = 1;
 }
 
-$stmt = $conn->prepare("SELECT COUNT(*) as total_posts FROM posts WHERE user_id = ?");
-$stmt->bind_param("i", $other_user_id);
+// $stmt = $conn->prepare("SELECT COUNT(*) as total_posts FROM posts WHERE user_id = ?");
+// $stmt->bind_param("i", $other_user_id);
+$conn = connect_PDO();
+$stmt = $conn->prepare("SELECT COUNT(*) FROM posts WHERE user_id = ?");
+$stmt->bindParam(1, $user_id, PDO::PARAM_INT);
 $stmt->execute();
-$stmt->bind_result($total_posts);
-$stmt->store_result();
-$stmt->fetch();
+// $stmt->bind_result($total_posts);
+// $stmt->store_result();
+$total_posts = $stmt->fetchColumn();
 
 $total_posts_per_page = 6;
 
@@ -27,10 +30,11 @@ $offset = ($page_no - 1) * $total_posts_per_page;
 $total_no_of_pages = ceil($total_posts / $total_posts_per_page);
 
 $stmt = $conn->prepare("SELECT * FROM posts WHERE user_id = ? ORDER BY id DESC LIMIT $offset, $total_posts_per_page"); 
-$stmt->bind_param("i", $other_user_id);
+// $stmt->bind_param("i", $other_user_id);
+$stmt->bindParam(1, $other_user_id, PDO::PARAM_INT);
 $stmt->execute();
-$posts = $stmt->get_result();
-
+// $posts = $stmt->get_result();
+$posts = $stmt->fetchAll();
 
 
 ?>
