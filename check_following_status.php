1fddbda49ca583bf9cad62f9f8193282576faf66
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 11:38:56 +0300

    folllowing status to PDO

--- check_following_status.php
+++ check_following_status.php
@@ -1,23 +1,25 @@
 <?php
 
-include('connection.php');
+// include('connection.php');
 
 $user_id = $_SESSION['id'];
 
 $stmt = $conn->prepare("SELECT * FROM followings WHERE user_id = ? AND other_user_id = ?");
-$stmt->bind_param("ii", $user_id, $other_user_id);
+// $stmt->bind_param("ii", $user_id, $other_user_id);
+$stmt->bindParam(1, $user_id, PDO::PARAM_INT);
+$stmt->bindParam(2, $other_user_id, PDO::PARAM_INT);
 $stmt->execute();
 
-$stmt->store_result();
-
-    if($stmt->num_rows() > 0)
-    {
-        $following_status = true;
-    }
-    else
-    {
-        $following_status = false;
-    }
+// $stmt->store_result();
+$result = $stmt->fetchAll();
+if($result)
+{
+    $following_status = true;
+}
+else
+{
+    $following_status = false;
+}
 
 //
 
