7328ca590097dd974b55c5fef824732f281b0c03
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 14:51:08 +0300

    delete_post PDO OK

--- delete_post.php
+++ delete_post.php
@@ -5,9 +5,10 @@ include('connection.php');
 if(isset($_POST['delete_post_btn'])) 
 {
     $post_id = $_POST['post_id'];
-
+    $conn = connect_PDO();
     $stmt = $conn->prepare("DELETE FROM posts WHERE id = ?");
-    $stmt->bind_param("i", $post_id);
+    // $stmt->bind_param("i", $post_id);
+    $stmt->bindParam(1, $post_id, PDO::PARAM_INT);
     if($stmt->execute())
     {
         header("location: profile.php?success_message=Post deleted");
