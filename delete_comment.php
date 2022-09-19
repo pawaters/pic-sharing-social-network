f449366cd18edf11f802719fc4846bd8b40fa1e5
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 14:48:06 +0300

    delete_comment PDO OK

--- delete_comment.php
+++ delete_comment.php
@@ -9,8 +9,10 @@ if(isset($_POST['delete_comment_btn'])){
     $comment_id = $_POST['comment_id'];
     $post_id = $_POST['post_id'];
 
+    $conn = connect_PDO();
     $stmt = $conn->prepare("DELETE FROM comments WHERE id = ?");
-    $stmt->bind_param("i",$comment_id);
+    // $stmt->bind_param("i",$comment_id);
+    $stmt->bindParam(1, $comment_id, PDO::PARAM_INT);
 
     if($stmt->execute()){
         header("location: single_post.php?post_id=".$post_id."&success_message=Comment deleted successfully");
