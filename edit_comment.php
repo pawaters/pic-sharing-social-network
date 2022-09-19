14d8d81059c8a6b917d9bd18f34e46c793cfc4db
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 14:54:22 +0300

    edit_comment PDO OK

--- edit_comment.php
+++ edit_comment.php
@@ -8,15 +8,17 @@
 
 
 		include('connection.php');
-
+        $conn = connect_PDO();
 		if(isset($_GET['comment_id']) && isset($_GET['post_id'])){
 
             $post_id = $_GET['post_id'];
 		    $comment_id = $_GET['comment_id'];
 		    $stmt = $conn->prepare("SELECT * FROM comments WHERE id = ?");
-		    $stmt->bind_param('i',$comment_id); 
+		    // $stmt->bind_param('i',$comment_id); 
+            $stmt->bindParam(1, $comment_id, PDO::PARAM_INT);
 		    $stmt->execute();
-		    $comment_array = $stmt->get_result();
+		    // $comment_array = $stmt->get_result();
+            $comment_array = $stmt->fetchAll();
 
 
 		}else{
