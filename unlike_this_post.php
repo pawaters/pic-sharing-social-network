9e919ff0117947429c9b6db222feb55acea77ebd
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-19 11:46:15 +0300

    unlike_this_post updated for PDO

--- unlike_this_post.php
+++ unlike_this_post.php
@@ -9,15 +9,18 @@ if(isset($_POST['heart_btn'])){
     $user_id = $_SESSION['id'];
     $post_id = $_POST['post_id'];
 
-
+    $conn = connect_PDO();
     //disssociate user with posts 
     $stmt = $conn->prepare("DELETE FROM likes WHERE user_id = ? AND post_id = ?");
-    $stmt->bind_param("ii",$user_id,$post_id);
+    // $stmt->bind_param("ii",$user_id,$post_id);
+    $stmt->bindParam(1, $user_id, PDO::PARAM_INT);
+    $stmt->bindParam(2, $post_id, PDO::PARAM_INT);
 
 
     //decrease number of likes of this post
     $stmt1 = $conn->prepare("UPDATE posts SET likes=likes-1 WHERE id = ?");
-    $stmt1->bind_param("i",$post_id);
+    // $stmt1->bind_param("i",$post_id);
+    $stmt1->bindParam(1, $post_id, PDO::PARAM_INT);
 
 
     $stmt->execute();
