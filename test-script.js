a6e1288b316958046d5fcf39f2852693cc59a880
pawaters <pierre-alban.waters@gmail.com>
2022-09-21 10:23:51 +0300

    img saved

--- test-script.js
+++ test-script.js
@@ -1,4 +1,6 @@
 const videoPlayer = document.querySelector("#player");
+const canvasElement = document.querySelector("#canvas");
+const captureButton = document.querySelector("#capture-btn");
 
 const startMedia = () => {
     // if (!("mediaDevices" in navigator)) {
@@ -26,7 +28,18 @@ const startMedia = () => {
         .getUserMedia({ video: true })
         .then(stream => {
         videoPlayer.srcObject = stream;
-        videoPlayer.style.display = "block";
+        /* videoPlayer.style.display = "block"; */
+    });
+
+    captureButton.addEventListener("click", event => {
+        //creates the object needed
+        const context = canvasElement.getContext("2d");
+        // draws in canvas
+        context.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+
+        // Convert the imae to a data-URL so it can be sabed
+        let picture = canvasElement.toDataURL();
+
     });
 }
 
