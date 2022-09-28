71edb5b5b22788e6072c8fc6264b97e291c1ea07
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-28 17:56:02 +0300

    Set up camera with kslotova example, to review

--- camera.php
+++ camera.php
@@ -64,6 +64,119 @@
         </div>
 
     </div>
+
+    <div class="thumbnails-box">
+					<p>🌟 Your previous awesome photos 🌟</p>
+						<?php
+							 
+							require_once('connection.php');
+							
+							$user_id = $_SESSION['id'];
+							$webcam = 1;
+
+							try {
+								$conn = connect_PDO();
+								$stmt = $conn->prepare("SELECT * FROM posts WHERE user_id = ? AND webcam = ? ORDER BY date DESC");
+								$stmt->bindParam(1, $user_id, PDO::PARAM_INT);
+								$stmt->bindParam(2, $webcam, PDO::PARAM_INT);
+								$stmt->execute();
+								$get_posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+							} catch (PDOException $error) {
+								echo $error->getMessage(); 
+								exit;
+							}
+							$conn = null;
+
+							foreach($get_posts as $post){ 
+						?>
+							<img src="<?php echo "assets/img/".$post['image']; ?>" alt="user-post">
+						<?php } ?>
+							
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>	
+
+		const filter = document.querySelectorAll(".sticker");
+		let camera_button = document.querySelector("#start-camera");
+		let video = document.querySelector("#video");
+		let capture_button = document.querySelector("#click-photo");
+		let canvas = document.querySelector("#canvas");
+
+		let sticker1 = document.getElementById("sticker1_path");
+		let sticker2 = document.getElementById("sticker2_path");
+		let sticker3 = document.getElementById("sticker3_path");
+		let sticker4 = document.getElementById("sticker4_path");
+		let sticker5 = document.getElementById("sticker5_path");
+		let sticker6 = document.getElementById("sticker6_path");
+
+		camera_button.disabled = true;
+		capture_button.disabled = true;
+
+		for(let i=0; i<filter.length; i++){
+			filter[i].addEventListener("click", (e) => {
+				camera_button.disabled = false;
+				capture_button.disabled = false;
+				myCanvas(e.target.id);
+			})
+		}
+		
+		camera_button.addEventListener('click', async function() {
+			let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+			video.classList.remove("is-hidden");
+			video.srcObject = stream;
+		});
+		
+		function myCanvas(sticker) {
+			let stickers_canvas = document.getElementById("stickers_canvas");
+			let stickers_ctx = stickers_canvas.getContext("2d");
+			let Selectedsticker = document.getElementById(sticker);
+
+			switch (sticker){
+				case 'sticker1':
+					stickers_ctx.drawImage(Selectedsticker, 30, 40, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker1.value = "assets/stickers/different.png"
+					break;
+				case 'sticker2':
+					stickers_ctx.drawImage(Selectedsticker, 300, 40, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker2.value = "assets/stickers/football.png"
+					break;
+				case 'sticker3':
+					stickers_ctx.drawImage(Selectedsticker, 150, 220, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker3.value = "assets/stickers/love.png"
+					break;
+				case 'sticker4':
+					stickers_ctx.drawImage(Selectedsticker, 170, 80, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker4.value = "assets/stickers/pink.png"
+					break;
+				case 'sticker5':
+					stickers_ctx.drawImage(Selectedsticker, 30, 200, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker5.value = "assets/stickers/rugby.png"
+					break;
+				case 'sticker6':
+					stickers_ctx.drawImage(Selectedsticker, 300, 200, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
+					sticker6.value = "assets/stickers/vibes.png"
+					break;
+			}
+			let stickersUrl = stickers_canvas.toDataURL();	
+			let finalStickers = document.getElementById("sticker-canvas");
+			finalStickers.value = stickersUrl;		
+		}
+
+		capture_button.addEventListener('click', function() {
+			let canvas = document.getElementById("canvas");
+			canvas.classList.remove("is-hidden");
+			stickers_canvas.classList.remove("is-hidden");
+			let ctx = canvas.getContext("2d");
+			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+			let canvasUrl = canvas.toDataURL();
+			let finalImage = document.getElementById("webcam-file");
+			finalImage.value = canvasUrl;
+		});
+
+	</script>
     
 </body>
 </html>
