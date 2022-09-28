e2d93eee33b51972ea449c001d9fbc5301e726fb
pawaters <pierre.alban.waters@gmail.com>
2022-09-28 11:33:04 +0300

    trying to display the decoded img

--- test-video.php
+++ test-video.php
@@ -18,8 +18,30 @@
         
     </form>
 
-    <?php 
-        print_r($_POST); 
+    <?php  
+        $webcam_file = $_POST['webcam_file'];
+        echo ("POST webcam_file\r\n<br/>");
+        print_r($webcam_file);
+        list($type, $data_url) = explode(';', $webcam_file);
+        list(, $data_url) = explode(',', $data_url);
+        echo ("<br/>type:\r\n<br/>");
+        print_r($type);
+        echo ("<br/>data_url:\r\n<br/>");
+        print_r($data_url);
+        $decoded_url = base64_decode($data_url);
+        // echo ("<br/>decoded_url:\r\n<br/>");
+        // print_r($decoded_url);
+        $webcam_img = imagecreatefromstring($decoded_url);
+        
+        //output the image
+        // header('Content-Type: image/png');
+        // imagepng($webcam_img);
+        // imagedestroy($webcam_img);
+        
+        echo $destination;
+        // $new_img = stamp_to_img($destination);
+        // echo $new_img;
+
     ?>
     <script src="test-script.js"></script>
 
