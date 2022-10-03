8222e365e425e5d1ac8d26fb6010fb493c86395f
pawaters <86101754+pawaters@users.noreply.github.com>
2022-10-03 17:39:31 +0300

    stop notif v1

---
+++ stop_notif.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+require_once 'connection.php';
+
+$user_id = $_SESSION['id'];
+
+try {
+    $sql = "UPDATE users SET notify = 0 WHERE id = ? LIMIT 1";
+    if (!$stmt = $conn->prepare($sql)) 
+    {
+        header("Location: verify.php?error_message=SQL error");
+        exit();
+    } else {
+        $stmt->bindParam(1, $user_id, PDO::PARAM_STR);  
+        $stmt->execute();
+        header("Location: index.php?success_message=You will no longer be notified!");
+        exit();
+    } 
+} catch (PDOException $error) {
+    echo $error->getMessage(); 
+    exit;
+}
+$conn = null;
+
+?>
+
+?>
