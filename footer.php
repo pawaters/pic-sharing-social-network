95d3789301482ec6639f9d91b9505eb2d8c047df
pawaters <86101754+pawaters@users.noreply.github.com>
2022-10-05 14:09:45 +0300

    footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         <a href="https://www.linkedin.com/in/pierrealbanwaters/">My Linkedin</a>
         <a href="https://github.com/pawaters">My Github</a>
         <a href="mailto:pierre.alban.waters@gmail.com">Email me</a>
+        <a href="logout.php">Logout</a>
         <!-- <a href="#">Terms</a> -->
         <!-- <a href="#" id="dark-btn">Dark/Light</a> -->
     </div>
