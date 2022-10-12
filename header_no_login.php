a71790fab72846407a59be88c7f94dcf088d9a4e
pawaters <pierre-alban.waters@gmail.com>
2022-10-12 11:12:45 +0300

    checked all if protected by logins

--- header_no_login.php
+++ header_no_login.php
@@ -4,16 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main page</title>
-    <!-- BOOTSTRAP CSS -->
+    <title>Camagru</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- FONTAWESOME CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <!--2 main divs: navbar and main 
-    each main div includes a wrapper CHANGE -->
     <nav class="navbar"> 
         <div class="nav-wrapper">
             <img src="assets/img/logo.jpg" class="brand-img"/>
--- liked_posts.php
+++ liked_posts.php
@@ -1,7 +1,4 @@
-
 <?php include('header.php'); ?>
-
-
 
 <?php 
 
