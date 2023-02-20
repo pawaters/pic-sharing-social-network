a32b1ece5b1e71bc273f9e595a6f00386f92f75f
pawaters <pierre.alban.waters@gmail.com>
2023-02-20 15:57:54 +0200

    edit db

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@
 ?>
 
 <!-- $DB_DSN = 'mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_7d09d8625973b2e';
-	$DB_USER = 'ba5f59dbcb8ee';
+	$DB_USER = 'ba5f59dbcb8eee';
 	$DB_PASSWORD = 'de2369e2';
 	$DB_HOST = 'mysql:host=eu-cdbr-west-03.cleardb.net'; -->
 
